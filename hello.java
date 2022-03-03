0c103fd9c177065c8df8bf93495199294d5a8fc9
umangdosi09 <87544808+umangdosi09@users.noreply.github.com>
2022-03-03 15:11:02 +0100

    Update hello.java
    first commit

--- hello.java
+++ hello.java
@@ -1,6 +1,7 @@
 public class hello {
             public static void main(String[] args) {
             // print
+                        // print
             System.out.println("hello,wasteful world");
             }
           }
